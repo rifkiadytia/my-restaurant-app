e271b83f173c0fdf124ba3e57acfa7f580738fe5
phamthai74@gmail.com <phamthai74@gmail.com@f780dc50-46c9-2cf2-9ee2-60bda98bc433>
2014-09-16 15:43:53 +0000

    update layout master for login ,search user, create user and admin

--- trunk/Source Code/Server/Restaurant/MVCInBuiltFeatures/App_Start/BundleConfig.cs
+++ trunk/Source Code/Server/Restaurant/MVCInBuiltFeatures/App_Start/BundleConfig.cs
@@ -18,8 +18,43 @@ namespace MVCInBuiltFeatures
                         "~/Content/jquery-ui/jquery-ui.css"
 
                         ));
-            //End 
-           
+            //End
+            //Admin script
+            //jquery
+            bundles.Add(new ScriptBundle("~/bundles/jquery-admin").Include(
+                        "~/Scripts/cpanel/jquery-1.9.1.min.js"
+                        ,"~/Scripts/cpanel/jquery-migrate-1.1.1.min.js"));
+            //jquery-ui
+             bundles.Add(new ScriptBundle("~/bundles/jquery-admin-ui").Include(
+                        "~/Scripts/cpanel/jquery-ui-1.9.2.min.js"));
+            //bootrap
+            bundles.Add(new ScriptBundle("~/bundles/jquery-response").Include(
+                        "~/Scripts/cpanel/modernizr.min.js"
+                        ,"~/Scripts/cpanel/bootstrap.min.js"));
+            //cookies
+            bundles.Add(new ScriptBundle("~/bundles/jquery-cookies").Include(
+                        "~/Scripts/cpanel/jquery.cookie.js"));
+            //custom
+            bundles.Add(new ScriptBundle("~/bundles/jquery-custom").Include(
+                        "~/Scripts/cpanel/custom.js"));
+            //uniform
+            bundles.Add(new ScriptBundle("~/bundles/jquery-uniform").Include(
+                        "~/Scripts/cpanel/jquery.uniform.min.js"
+                        ,"~/Scripts/cpanel/flot/jquery.flot.min.js"
+                        ,"~/Scripts/cpanel/flot/jquery.flot.resize.min.js"
+                        ,"~/Scripts/cpanel/responsive-tables.js"));
+            //excanvas
+            bundles.Add(new ScriptBundle("~/bundles/jquery-excanvas").Include(
+                        "~/Scripts/cpanel/excanvas.min.js"));
+            //End admin template script
+            //Admin css
+            bundles.Add(new StyleBundle("~/Content/admin-css").Include(
+                      "~/Content/cpanel/css/style.default.css",
+                      "~/Content/cpanel/css/style.shinyblue.css"));
+            bundles.Add(new StyleBundle("~/Content/admin-table-css").Include(
+                      "~/Content/cpanel/css/responsive-tables.css"));
+            
+            //End admin css
             bundles.Add(new ScriptBundle("~/bundles/jquery-ui").Include(
                         "~/Scripts/jquery-ui-{version}.js"));
 
