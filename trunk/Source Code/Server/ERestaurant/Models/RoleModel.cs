405d5846318526866e6ca68490b17b3ecf4da37d
phamthai74@gmail.com <phamthai74@gmail.com@f780dc50-46c9-2cf2-9ee2-60bda98bc433>
2014-09-19 17:08:57 +0000

    add

---
+++ trunk/Source Code/Server/ERestaurant/Models/RoleModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Globalization;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace ERestaurant.Models
+{
+    public class RoleModel
+    {
+        [Required]
+        public string RoleName { get; set; }
+    }
+}
