96b8d25f4b279d306f41abbcd79e9da412a549eb
phamthai74@gmail.com <phamthai74@gmail.com@f780dc50-46c9-2cf2-9ee2-60bda98bc433>
2014-09-19 16:21:47 +0000

    add new

---
+++ trunk/Source Code/Server/ERestaurant/Dataservice/DataConnection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ERestaurant.Models;
+
+namespace ERestaurant.Dataservice
+{
+    public class DataConnection
+    {
+        private static volatile RestaurantInsightEntities instance;
+        private static object syncRoot = new Object();
+
+        private DataConnection() { }
+
+        public static RestaurantInsightEntities Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                            instance = new RestaurantInsightEntities();
+                    }
+                }
+                return instance;
+            }
+        }
+    }
+}
