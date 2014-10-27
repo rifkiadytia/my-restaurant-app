9e66f31c3385c8837719e074d86a332f7dfb17b9
phamthai74@gmail.com <phamthai74@gmail.com@f780dc50-46c9-2cf2-9ee2-60bda98bc433>
2014-10-27 16:18:14 +0000

    update

---
+++ trunk/Source Code/Client/ERestaurant/ERestaurantClient/ERestaurantClient.Shared/views/Table.js
@@ -0,0 +1,78 @@
+﻿
+var ORDERED_STATUS = "1";
+var EMPTY_STATUS = "0";
+
+ERestaurantClient.Table = function (params) {
+    $(".dx-scrollable-content-horizontal").attr('style', 'left:0px;');
+    
+    var viewModel = {
+        dSource: tileViewData,
+        GetBackground: function (e) {
+            if (e === ORDERED_STATUS) {
+                return "url('/images/table.jpg')";
+            }
+            else {
+                return "url('/images/table_ordered.png')";
+            }
+        },
+        //dSource: ERestaurantClient.db.sampleData.Categories,
+        onItemClick: function (e) {
+            var categoryName = e.itemData.name;
+            alert(categoryName);
+        }
+    };
+    return viewModel;
+};
+scrollBarVisible = ko.observable(false);
+var tileViewData = [
+      { name: "Alabama", tableStatus: "1", population: 4822023, area: 135765 },
+      { name: "Alaska", tableStatus: "Juneau", population: 731449, area: 1717854 },
+      { name: "Arizona", tableStatus: "Phoenix", population: 6553255, area: 295254 },
+      { name: "Arkansas", tableStatus: "Little Rock", population: 2949131, area: 137002 },
+      { name: "California", tableStatus: "Sacramento", population: 38041430, area: 423970 },
+      { name: "Colorado", tableStatus: "Denver", population: 5187582, area: 269602 },
+      { name: "Connecticut", tableStatus: "Hartford", population: 3590347, area: 14356 },
+      { name: "Connecticut", tableStatus: "Hartford", population: 3590347, area: 14356 },
+      { name: "Connecticut", tableStatus: "1", population: 3590347, area: 14356 },
+      { name: "Connecticut", tableStatus: "Hartford", population: 3590347, area: 14356 },
+      { name: "Connecticut", tableStatus: "Hartford", population: 3590347, area: 14356 },
+      { name: "Connecticut", tableStatus: "Hartford", population: 3590347, area: 14356 },
+      { name: "Connecticut", tableStatus: "Hartford", population: 3590347, area: 14356 },
+      { name: "Connecticut", tableStatus: "Hartford", population: 3590347, area: 14356 },
+       { name: "Alabama", tableStatus: "1", population: 4822023, area: 135765 },
+      { name: "Alaska", tableStatus: "Juneau", population: 731449, area: 1717854 },
+      { name: "Arizona", tableStatus: "Phoenix", population: 6553255, area: 295254 },
+      { name: "Arkansas", tableStatus: "Little Rock", population: 2949131, area: 137002 },
+      { name: "California", tableStatus: "Sacramento", population: 38041430, area: 423970 },
+      { name: "Colorado", tableStatus: "Denver", population: 5187582, area: 269602 },
+       { name: "Alabama", tableStatus: "1", population: 4822023, area: 135765 },
+      { name: "Alaska", tableStatus: "Juneau", population: 731449, area: 1717854 },
+      { name: "Arizona", tableStatus: "1", population: 6553255, area: 295254 },
+      { name: "Arkansas", tableStatus: "Little Rock", population: 2949131, area: 137002 },
+      { name: "California", tableStatus: "Sacramento", population: 38041430, area: 423970 },
+      { name: "Colorado", tableStatus: "Denver", population: 5187582, area: 269602 },
+       { name: "Alabama", tableStatus: "1", population: 4822023, area: 135765 },
+      { name: "Alaska", tableStatus: "Juneau", population: 731449, area: 1717854 },
+      { name: "Arizona", tableStatus: "1", population: 6553255, area: 295254 },
+      { name: "Arkansas", tableStatus: "Little Rock", population: 2949131, area: 137002 },
+      { name: "California", tableStatus: "Sacramento", population: 38041430, area: 423970 },
+      { name: "Colorado", tableStatus: "Denver", population: 5187582, area: 269602 },
+      { name: "Connecticut", tableStatus: "Hartford", population: 3590347, area: 14356 },
+      { name: "Connecticut", tableStatus: "Hartford", population: 3590347, area: 14356 },
+      { name: "Connecticut", tableStatus: "Hartford", population: 3590347, area: 14356 },
+      { name: "Connecticut", tableStatus: "Hartford", population: 3590347, area: 14356 },
+      { name: "Colorado", tableStatus: "Denver", population: 5187582, area: 269602 },
+      { name: "Connecticut", tableStatus: "Hartford", population: 3590347, area: 14356 },
+      { name: "Connecticut", tableStatus: "Hartford", population: 3590347, area: 14356 },
+      { name: "Connecticut", tableStatus: "1", population: 3590347, area: 14356 },
+      { name: "Connecticut", tableStatus: "Hartford", population: 3590347, area: 14356 },
+      { name: "Connecticut", tableStatus: "Hartford", population: 3590347, area: 14356 },
+      { name: "Connecticut", tableStatus: "1", population: 3590347, area: 14356 },
+      { name: "Connecticut", tableStatus: "1", population: 3590347, area: 14356 },
+      { name: "Connecticut", tableStatus: "Hartford", population: 3590347, area: 14356 },
+      { name: "Connecticut", tableStatus: "1", population: 3590347, area: 14356 },
+      { name: "Connecticut", tableStatus: "Hartford", population: 3590347, area: 14356 },
+      { name: "Connecticut", tableStatus: "Hartford", population: 3590347, area: 14356 },
+      { name: "Connecticut", tableStatus: "1", population: 3590347, area: 14356 },
+      { name: "Connecticut", tableStatus: "Hartford", population: 3590347, area: 14356 }
+];
